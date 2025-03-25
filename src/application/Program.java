40a0f7ccfe92f172e799e26c2f7cd53da2e4778c
wescaetano <weslleycaetanosoares@gmail.com>
2025-03-25 19:43:27 -0300

    Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -42,6 +42,12 @@ public class Program {
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id  = " + newSeller.getId());
 
+
+        System.out.println("=== TEST 5: seller update ===");
+        seller = sellerDao.findById(1);
+        seller.setName("Bob Brown");
+        sellerDao.update(seller);
+        System.out.println("Update completed!");
         }
     }
 
