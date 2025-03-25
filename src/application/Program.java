5c15d4ed4431617b1be39110d12dd764cc288eb8
wescaetano <weslleycaetanosoares@gmail.com>
2025-03-25 16:44:14 -0300

    Insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -35,6 +35,13 @@ public class Program {
         for(Seller sell : list){
             System.out.println(sell);
         }
+
+        System.out.println();
+        System.out.println("=== TEST 4: seller insert ===");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id  = " + newSeller.getId());
+
         }
     }
 
